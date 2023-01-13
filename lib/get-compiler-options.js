simonhaenisch/prettier-plugin-organize-imports

commit 290a1fda10a572eccd1aa57a33e39128697a8dab

feat: support pug templates in Vue files (#79)

lib/get-compiler-options.js
@@ -1,15 +1,14 @@
 const { dirname } = require('path');
 const ts = require('typescript');
+const { memoize } = require('./memoize');
 
 /**
- * Get the compiler options from a tsconfig path. Returns from in-memory cache if they have been read already.
+ * Get the compiler options from a path to a file in the project. Returns from in-memory cache if they have been read already.
+ *
+ * @param {string} path path to a file in the project
  */
-module.exports.getCompilerOptions = (tsconfig = '') => {
-	const cachedCompilerOptions = compilerOptionsCache.get(tsconfig);
-
-	if (cachedCompilerOptions) {
-		return cachedCompilerOptions;
-	}
+function getCompilerOptions(path) {
+	const tsconfig = findTsconfig(path);
 
 	const compilerOptions = tsconfig
 		? ts.parseJsonConfigFileContent(ts.readConfigFile(tsconfig, ts.sys.readFile).config, ts.sys, dirname(tsconfig))
@@ -18,12 +17,35 @@ module.exports.getCompilerOptions = (tsconfig = '') => {
 
 	compilerOptions.allowJs = true; // for automatic JS support
 
-	compilerOptionsCache.set(tsconfig, compilerOptions);
-
 	return compilerOptions;
-};
+}
+
+module.exports.getCompilerOptions = memoize(getCompilerOptions);
 
 /**
- * @type {Map<string, ts.CompilerOptions>}
+ * Get the Vue compiler options from a path to a file in the project.
+ *
+ * @param {string} path path to a file in the project
  */
-const compilerOptionsCache = new Map();
+function getVueCompilerOptions(path) {
+	const tsconfig = findTsconfig(path);
+
+	return tsconfig
+		? require('@volar/vue-language-core').createParsedCommandLine(
+				// @ts-ignore
+				ts,
+				ts.sys,
+				tsconfig,
+				[],
+		  ).vueOptions
+		: {};
+}
+
+module.exports.getVueCompilerOptions = memoize(getVueCompilerOptions);
+
+/**
+ * Find the path of the project's tsconfig from a path to a file in the project.
+ *
+ * @type {(path: string) => string | undefined}
+ */
+const findTsconfig = memoize((path) => ts.findConfigFile(path, ts.sys.fileExists));

lib/service-host.js
@@ -1,6 +1,6 @@
 const ts = require('typescript');
 
-const { getCompilerOptions } = require('./get-compiler-options');
+const { getCompilerOptions, getVueCompilerOptions } = require('./get-compiler-options');
 
 /**
  * Create the most basic TS language service host for the given file to make import sorting work.
@@ -10,10 +10,8 @@ const { getCompilerOptions } = require('./get-compiler-options');
  *
  * @returns {ts.LanguageServiceHost}
  */
-const getTypeScriptLanguageServiceHost = (path, content) => {
-	const tsconfig = ts.findConfigFile(path, ts.sys.fileExists);
-
-	const compilerOptions = getCompilerOptions(tsconfig);
+function getTypeScriptLanguageServiceHost(path, content) {
+	const compilerOptions = getCompilerOptions(path);
 
 	return {
 		directoryExists: ts.sys.directoryExists,
@@ -33,7 +31,7 @@ const getTypeScriptLanguageServiceHost = (path, content) => {
 			}
 		},
 	};
-};
+}
 
 /**
  * Get a Language Service Host for Vue support, that has some extra methods needed by `@volar/vue-typescript`.
@@ -45,17 +43,21 @@ const getTypeScriptLanguageServiceHost = (path, content) => {
  *
  * @returns {VueLanguageServiceHost}
  */
-const getVueLanguageServiceHost = (path, content) => ({
-	...getTypeScriptLanguageServiceHost(path, content),
-	getVueCompilationSettings: () => ({}),
-	/**
-	 * Can return either `require('typescript')` or `require('typescript/lib/tsserverlibrary')`.
-	 *
-	 * @see https://github.com/simonhaenisch/prettier-plugin-organize-imports/pull/60#discussion_r934408179
-	 *
-	 * @returns {any}
-	 */
-	getTypeScriptModule: () => ts,
-});
+function getVueLanguageServiceHost(path, content) {
+	const vueCompilerOptions = getVueCompilerOptions(path);
+
+	return {
+		...getTypeScriptLanguageServiceHost(path, content),
+		getVueCompilationSettings: () => vueCompilerOptions,
+		/**
+		 * Can return either `require('typescript')` or `require('typescript/lib/tsserverlibrary')`.
+		 *
+		 * @see https://github.com/simonhaenisch/prettier-plugin-organize-imports/pull/60#discussion_r934408179
+		 *
+		 * @returns {any}
+		 */
+		getTypeScriptModule: () => ts,
+	};
+}
 
 module.exports = { getTypeScriptLanguageServiceHost, getVueLanguageServiceHost };

.github/workflows/test.yml
@@ -7,7 +7,7 @@ jobs:
     strategy:
       matrix:
         os: ['ubuntu-latest', 'macos-latest', 'windows-latest']
-        node: [12, 14, 16, 18]
+        node: [14, 16, 18]
 
     runs-on: ${{ matrix.os }}
 

lib/memoize.js
@@ -1,0 +1,23 @@
+/**
+ * Simple memoization utility that only uses the first argument as cache key and has no memory limit.
+ *
+ * @template {(...args: any[]) => any} F
+ * @param {F} f
+ * @returns {F}
+ */
+module.exports.memoize = (f) => {
+	const cache = new Map();
+
+	// @ts-ignore
+	return function (cacheKey, ...rest) {
+		if (cache.has(cacheKey)) {
+			return cache.get(cacheKey);
+		}
+
+		const result = f(cacheKey, ...rest);
+
+		cache.set(cacheKey, result);
+
+		return result;
+	};
+};

test.js
@@ -167,6 +167,21 @@ import { NDivider } from "naive-ui";
 	t.is(formattedCode, code);
 });
 
+test('works with pug templates in Vue files', (t) => {
+	const code = `<script setup lang="ts">
+import Foo from "@/components";
+</script>
+
+<template lang="pug">
+Foo
+</template>
+`;
+
+	const formattedCode = prettify(code, { filepath: 'file.vue' });
+
+	t.is(formattedCode, code);
+});
+
 test('does not remove unused imports with `organizeImportsSkipDestructiveCodeActions` enabled', (t) => {
 	const code = `import { foo } from "./bar";
 `;

commit 691c3edd4cf8336e9a9fded1cb8b46bbf95aa90a

fix: performance regression introduced by 290a1fd (PR #79)
There was an unintentional change to use the file path instead of tsconfig path for memoization, which changes for every file (whereas the tsconfig path is often the same for the whole project).

Fixes #90.

lib/get-compiler-options.js
@@ -4,13 +4,11 @@ const { findTsconfig } = require('./find-tsconfig');
 const { memoize } = require('./memoize');
 
 /**
- * Get the compiler options from a path to a file in the project.
+ * Get the compiler options from the path to a tsconfig.
  *
- * @param {string} path path to a file in the project
+ * @param {string | undefined} tsconfig path to tsconfig
  */
-function getCompilerOptions(path) {
-	const tsconfig = findTsconfig(path);
-
+function getCompilerOptions(tsconfig) {
 	const compilerOptions = tsconfig
 		? ts.parseJsonConfigFileContent(ts.readConfigFile(tsconfig, ts.sys.readFile).config, ts.sys, dirname(tsconfig))
 				.options
@@ -24,15 +22,13 @@ function getCompilerOptions(path) {
 module.exports.getCompilerOptions = memoize(getCompilerOptions);
 
 /**
- * Get the Vue compiler options from a path to a file in the project.
+ * Get the Vue compiler options from the path to a tsconfig.
  *
  * Uses a dynamic require instead of top-level because this is only needed for Vue.
  *
- * @param {string} path path to a file in the project
+ * @param {string | undefined} tsconfig path to tsconfig
  */
-function getVueCompilerOptions(path) {
-	const tsconfig = findTsconfig(path);
-
+function getVueCompilerOptions(tsconfig) {
 	return tsconfig
 		? require('@volar/vue-language-core').createParsedCommandLine(
 				// @ts-ignore

lib/service-host.js
@@ -14,7 +14,7 @@ const { getCompilerOptions, getVueCompilerOptions } = require('./get-compiler-op
  */
 function getTypeScriptLanguageServiceHost(path, content) {
 	const tsconfig = findTsconfig(path);
-	const compilerOptions = getCompilerOptions(path);
+	const compilerOptions = getCompilerOptions(tsconfig);
 
 	return {
 		directoryExists: ts.sys.directoryExists,
@@ -47,7 +47,8 @@ function getTypeScriptLanguageServiceHost(path, content) {
  * @returns {VueLanguageServiceHost}
  */
 function getVueLanguageServiceHost(path, content) {
-	const vueCompilerOptions = getVueCompilerOptions(path);
+	const tsconfig = findTsconfig(path);
+	const vueCompilerOptions = getVueCompilerOptions(tsconfig);
 
 	return {
 		...getTypeScriptLanguageServiceHost(path, content),