59c5329c468a48c1ed0fecf9e6690cce1090cc34
simonhaenisch <simonhaenisch@users.noreply.github.com>
2024-07-02 13:57:48 +0200

    chore(tests): upgrade ava from v3 to v6

--- test/_utils.js
+++ test/_utils.js
@@ -1,4 +1,5 @@
 const prettier = require('prettier');
+const { macro } = require('ava').default;
 
 /**
  * @param {string} code
@@ -10,25 +11,20 @@ module.exports.prettify = async (code, options) =>
 /**
  * @param {prettier.Options['parser']} parser
  */
-module.exports.getMacro = (parser) => {
-	/**
-	 * @param {import('ava').Assertions} t
-	 * @param {string} input
-	 * @param {string} expected
-	 * @param {object} options
-	 * @param {prettier.Options} [options.options]
-	 * @param {(res: string) => string} [options.transformer]
-	 */
-	async function macro(t, input, expected, { options = {}, transformer = (res) => res.split('\n')[0] } = {}) {
-		const formattedCode = await module.exports.prettify(input, { parser, ...options });
+module.exports.getMacro = (parser) =>
+	macro({
+		/**
+		 * @param {import('ava').ExecutionContext} t
+		 * @param {string} input
+		 * @param {string} expected
+		 * @param {{ options?: import('prettier').Options; transformer?: (res: string) => string }} [options]
+		 */
+		async exec(t, input, expected, { options = {}, transformer = (res) => res.split('\n')[0] } = {}) {
+			const formattedCode = await module.exports.prettify(input, { parser, ...options });
 
-		t.is(transformer(formattedCode), expected);
-	}
-
-	/**
-	 * @param {string} title
-	 */
-	macro.title = (title) => `[${parser}] ${title}`;
-
-	return macro;
-};
+			t.is(transformer(formattedCode), expected);
+		},
+		title(providedTitle = '<missing-title>') {
+			return `[${parser}] ${providedTitle}`;
+		},
+	});
--- test/main.js
+++ test/main.js
@@ -1,74 +1,73 @@
 const { getMacro, prettify } = require('./_utils');
 const test = require('ava').default;
 
-/**
- * @type {import('ava').OneOrMoreMacros<[string, string] | [string, string, { options?: import('prettier').Options, transformer?: (res: string) => string }], unknown>}
- */
-const macros = [getMacro('typescript'), getMacro('babel'), getMacro('babel-ts')];
+const macros = ['typescript', 'babel', 'babel-ts'].map((parser) => getMacro(parser));
 
-test(
-	'sorts imports',
-	macros,
-	`
-		import { foo, bar } from "foobar"
+for (const macro of macros) {
+	test(
+		'sorts imports',
+		macro,
+		`
+			import { foo, bar } from "foobar"
 
-		export const foobar = foo + bar
-	`,
-	'import { bar, foo } from "foobar";',
-);
+			export const foobar = foo + bar
+		`,
+		'import { bar, foo } from "foobar";',
+	);
 
-test(
-	'removes partially unused imports',
-	macros,
-	`
-		import { foo, bar, baz } from "foobar";
+	test(
+		'removes partially unused imports',
+		macro,
+		`
+			import { foo, bar, baz } from "foobar";
 
-		const foobar = foo + baz
-	`,
-	'import { baz, foo } from "foobar";',
-);
+			const foobar = foo + baz
+		`,
+		'import { baz, foo } from "foobar";',
+	);
 
-test('removes completely unused imports', macros, 'import { foo } from "foobar"', '');
+	test('removes completely unused imports', macro, 'import { foo } from "foobar"', '');
 
-test(
-	'works with multi-line imports',
-	macros,
-	`
-		import {
-			foo,
-			bar,
-			baz,
-		} from "foobar";
+	test(
+		'works with multi-line imports',
+		macro,
+		`
+			import {
+				foo,
+				bar,
+				baz,
+			} from "foobar";
 
-		console.log({ foo, bar, baz });
-	`,
-	'import { bar, baz, foo } from "foobar";',
-);
+			console.log({ foo, bar, baz });
+		`,
+		'import { bar, baz, foo } from "foobar";',
+	);
 
-test(
-	'works without a filepath',
-	macros,
-	`
-		import { foo, bar } from "foobar"
+	test(
+		'works without a filepath',
+		macro,
+		`
+			import { foo, bar } from "foobar"
 
-		export const foobar = foo + bar
-	`,
-	'import { bar, foo } from "foobar";',
-	{ options: { filepath: undefined } },
-);
+			export const foobar = foo + bar
+		`,
+		'import { bar, foo } from "foobar";',
+		{ options: { filepath: undefined } },
+	);
 
-test(
-	'files with `// organize-imports-ignore` are skipped',
-	macros,
-	`
-		// organize-imports-ignore
-		import { foo, bar } from "foobar"
+	test(
+		'files with `// organize-imports-ignore` are skipped',
+		macro,
+		`
+			// organize-imports-ignore
+			import { foo, bar } from "foobar"
 
-		export const foobar = foo + bar
-	`,
-	'import { foo, bar } from "foobar";',
-	{ transformer: (res) => res.split('\n')[1] },
-);
+			export const foobar = foo + bar
+		`,
+		'import { foo, bar } from "foobar";',
+		{ transformer: (res) => res.split('\n')[1] },
+	);
+}
 
 test('skips when formatting a range', async (t) => {
 	const code = 'import { foo } from "./bar";';
